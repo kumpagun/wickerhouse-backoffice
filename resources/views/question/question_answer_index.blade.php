b441e101e2f90532211ceb450407e042f1c62d7a
Sorachai Bunjerdveha <SorachaiZa007@CanUCMe.local>
2020-01-10 16:35:22 +0700

    question

---
+++ resources/views/question/question_answer_index.blade.php
@@ -0,0 +1,121 @@
+@extends('layouts.app')
+
+@php $title = $course->title @endphp
+
+@section('content-header-left')
+<h3 class="content-header-title mb-2">{{ $title }}</h3>
+<div class="row breadcrumbs-top">
+  <div class="breadcrumb-wrapper col-12">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="{{ route('question_index') }}">Question</a></li>
+      <li class="breadcrumb-item active">{{ $title }}</li>
+    </ol>
+  </div>
+</div>
+@endsection
+
+@section('content')
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-10 col-lg-8 col-xl-8">
+      <div class="card">
+        <div class="card-header pb-0">
+          <h4 class="card-title">{{ $title }}</h4>
+          <div class="heading-elements">
+            <ul class="list-inline mb-0">
+              <li>
+                <a href="{{ route('question_answer_index', ['course_id' => $course->_id]) }}">
+                  <button class="badge badge @if(empty($type)) badge-primary @else badge-secondary @endif badge-pill">ทั้งหมด</button>
+                </a>
+              </li>
+              <li>
+                <a href="{{ route('question_answer_index', ['course_id' => $course->_id, 'type' => 'no_answer']) }}">
+                  <button class="badge badge @if(!empty($type) && $type=='no_answer') badge-primary @else badge-secondary @endif badge-pill">ยังไม่ได้ตอบ</button>
+                </a>
+              </li>
+              <li>
+                <a href="{{ route('question_answer_index', ['course_id' => $course->_id, 'type' => 'answer']) }}">
+                  <button class="badge badge @if(!empty($type) && $type=='answer') badge-primary @else badge-secondary @endif badge-pill">ตอบแล้ว</button>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-hover">
+              <tr>
+                <th class="text-center table-no">#</th>
+                <th class="text-center">คำถาม</th>
+                <th class="text-center">Result</th>
+              </tr>
+              @if(count($datas)>0)
+              @foreach ($datas as $item)
+                <tr>
+                  <td class="text-center">{{ $loop->iteration }}</td>
+                  <td>  
+                    <a href="#" data-toggle="modal" data-target="#answer-{{$item->_id}}">{{ $item->question }}</a>
+                  </td>
+                  @if(!empty($item->answer))
+                  <td class="text-center text-success">ตอบแล้ว</td>
+                  @else
+                  <td class="text-center text-warning">ยังไม่ได้ตอบ</td>
+                  @endif
+                </tr>
+              @endforeach
+              @else
+                <tr><td colspan="99" class="text-center">ไม่มีข้อมูล</td></tr>
+              @endif
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  @foreach ($datas as $item)
+    <div class="modal fade text-left" id="answer-{{$item->_id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1" style="display: none;" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title" id="myModalLabel1">ตอบคำถาม</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <form class="form-horizontal" action="{{ route('question_answer_store') }}" method="POST">
+            @csrf
+            <input type="hidden" name="question_answer_id" value="{{ $item->_id }}" />
+            <div class="modal-body">
+              <div class="row mb-2">
+                <div class="col-12"><strong>คำถาม</strong></div>
+                <div class="col-12">{!! $item->question !!}</div>
+              </div>
+              <div class="row skin skin-square mb-2">
+                <div class="col-12 mb-2">
+                  <strong for="user-status">คำตอบ</strong>
+                  <textarea name="answer" class="form-control mt-1" rows="10">{{ $item->answer }}</textarea>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn grey btn-outline-secondary" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-outline-primary">Save changes</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  @endforeach
+@endsection
+
+@section('style')
+  <link rel="stylesheet" type="text/css" href="{{ asset('stack-admin/app-assets/vendors/css/forms/icheck/icheck.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('stack-admin/app-assets/vendors/css/forms/icheck/custom.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('stack-admin/app-assets/css/plugins/forms/checkboxes-radios.css') }}">
+@endsection
+
+@section('script')
+  <script src="{{ asset('stack-admin/app-assets/js/scripts/forms/select/form-select2.js') }}" type="text/javascript"></script>
+  <script src="{{ asset('stack-admin/app-assets/vendors/js/forms/icheck/icheck.min.js') }}" type="text/javascript"></script>
+  <script src="{{ asset('stack-admin/app-assets/js/scripts/forms/checkbox-radio.js') }}" type="text/javascript"></script>
+@endsection
---
+++ resources/views/question/question_index.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@php $title = strtoupper('Q&A'); @endphp
+
+@section('content-header-left')
+<h3 class="content-header-title mb-2">{{ $title }}</h3>
+<div class="row breadcrumbs-top">
+  <div class="breadcrumb-wrapper col-12">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item active">{{ $title }}</li>
+    </ol>
+  </div>
+</div>
+@endsection
+
+@section('content')
+<div class="row justify-content-center">
+  <div class="col-12 col-md-8 col-lg-8 col-xl-6">
+    <div class="card">
+      <div class="card-header pb-0">
+        <h4 class="card-title">{{ $title }}</h4>
+      </div>
+      <div class="card-body">
+        <div class="table-responsive">
+          <table class="table table-hover">
+            <tr>
+              <th class="text-center table-no">#</th>
+              <th class="text-center">Course Name</th>
+              <th class="text-center">จำนวนคนถาม / ตอบคำถาม</th>
+            </tr>
+            @if(count($datas)>0)
+            @foreach ($datas as $item)
+              @php $total_question = CourseClass::get_question_total($item->_id); @endphp
+              @php $total_question_answer = CourseClass::get_question_answer_total($item->_id); @endphp
+              @if(!empty($total_question)) 
+              <tr>
+                <td><a href="{{ route('question_answer_index',['question_id' => $item->_id]) }}">{{ $loop->iteration }}</a></td>
+                <td><a href="{{ route('question_answer_index',['question_id' => $item->_id]) }}">{{ $item->title }}</a></td>
+                <td class="text-center">{{ number_format($total_question).' / '.number_format($total_question_answer) }}</td>
+              </tr>
+              @else
+              <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $item->title }}</td>
+                <td class="text-center">{{ number_format($total_question).' / '.number_format($total_question_answer) }}</td>
+              </tr>
+              @endif
+            @endforeach
+            @else
+              <tr><td colspan="2" class="text-center">ไม่มีข้อมูล</td></tr>
+            @endif
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
