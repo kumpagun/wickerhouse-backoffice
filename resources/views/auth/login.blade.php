ffdbf2ad6e72100275d8f889d1f395136b1e19f2
Sorachai Bunjerdveha <SorachaiZa007@CanUCMe.local>
2020-01-22 14:09:41 +0700

    logo has

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,9 +29,9 @@
       <div class="card border-grey border-lighten-3 m-0">
         <div class="card-header border-0">
           <div class="card-title text-center">
-            {{-- <div class="p-1">
-              <img src="{{ asset('stack-admin/app-assets/images/logo/stack-logo-dark.png') }}" alt="branding logo">
-            </div> --}}
+            <div class="p-1">
+              <img src="{{ asset('/images/jaslogo.png') }}" class="w-50" alt="branding logo">
+            </div>
           </div>
           <h6 class="card-subtitle line-on-side text-muted text-center font-small-3 pt-2">
             <span>Login with Online Learning</span>
