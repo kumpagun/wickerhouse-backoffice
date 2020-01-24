1200346c9c4b4a0eb563aac3c915ab038eb8d062
Sorachai Bunjerdveha <SorachaiZa007@CanUCMe.local>
2020-01-24 14:11:56 +0700

    remove ep type

--- app/Jobs/UploadClip.php
+++ app/Jobs/UploadClip.php
@@ -41,23 +41,14 @@ class UploadClip implements ShouldQueue
     $client = new Client(['headers' => ['Authorization' => 'Bearer 4styVfeWRWtLEqZYu5v53me1qIODJ6W6RLwkiPQVFPxwdxGHb1oHC2hHwArF']]);
     try {
       $source = Storage::disk('local')->readStream($this->path);
-      if ($this->episode->type == 'preview') {
-        $params = [
-          ['name' => 'service',       'contents' => 'jasonline_clear'],
-          ['name' => 'source',        'contents' => $source],
-          ['name' => 'priority',      'contents' => 'default'],
-          ['name' => 'callback_url',  'contents' => 'https://bo.tutorme.in.th/api/course/transcode/callback']
-        ];
-      } else if (in_array($this->episode->type, ['free', 'payment'])) {
-        $content_id = $this->episode->content_id;
-        $params = [
-          ['name' => 'service',       'contents' => 'jasonline_drm'],
-          ['name' => 'source',        'contents' => $source],
-          ['name' => 'priority',      'contents' => 'default'],
-          ['name' => 'callback_url',  'contents' => 'https://bo.tutorme.in.th/api/course/transcode/callback'],
-          ['name' => 'content_id',    'contents' => $content_id]
-        ];
-      }
+      $content_id = $this->episode->content_id;
+      $params = [
+        ['name' => 'service',       'contents' => 'jasonline_drm'],
+        ['name' => 'source',        'contents' => $source],
+        ['name' => 'priority',      'contents' => 'default'],
+        ['name' => 'callback_url',  'contents' => 'https://bo.tutorme.in.th/api/course/transcode/callback'],
+        ['name' => 'content_id',    'contents' => $content_id]
+      ];
 
       $transcode_url = 'http://upload-seeme.mthai.com/api/transcode';
       $res = $client->request('POST', $transcode_url, [
@@ -68,29 +59,29 @@ class UploadClip implements ShouldQueue
       $body = $res->getBody();
 
       if ($status_code == 200) {
-          $data = json_decode($body, true);
-          if (is_array($data)) {
-            $this->episode->task_id = $data['id'];
-            $streams = [];
-            foreach ($data['qualities'] as $key => $value) {
-              $streams[$key] = ['quality' => $value];
-            }
-            $this->episode->streams = $streams;
-            $this->episode->meta = $data['meta'];
-            $this->episode->duration = array_get($data, 'meta.format.duration', 0);
-            $this->episode->transcode_status = 'queued';
-            $this->episode->save();
-
-            $disk = Storage::disk('local');
-            if ($disk->exists($this->path)) {
-              $disk->delete($this->path);
-            }
-          } else {
-            Log::info($body);
-            Log::error('[Error] Failed uploading clip id = '.(string)$this->episode->_id.' / Status = '.$status_code);
-            $this->episode->transcode_status = 'failed';
-            $this->episode->save();
+        $data = json_decode($body, true);
+        if (is_array($data)) {
+          $this->episode->task_id = $data['id'];
+          $streams = [];
+          foreach ($data['qualities'] as $key => $value) {
+            $streams[$key] = ['quality' => $value];
           }
+          $this->episode->streams = $streams;
+          $this->episode->meta = $data['meta'];
+          $this->episode->duration = array_get($data, 'meta.format.duration', 0);
+          $this->episode->transcode_status = 'queued';
+          $this->episode->save();
+
+          $disk = Storage::disk('local');
+          if ($disk->exists($this->path)) {
+            $disk->delete($this->path);
+          }
+        } else {
+          Log::info($body);
+          Log::error('[Error] Failed uploading clip id = '.(string)$this->episode->_id.' / Status = '.$status_code);
+          $this->episode->transcode_status = 'failed';
+          $this->episode->save();
+        }
       } else {
         Log::error('[Cannot upload] Failed uploading clip id = '.(string)$this->episode->_id.' / Status = '.$status_code);
         $this->episode->transcode_status = 'failed';
